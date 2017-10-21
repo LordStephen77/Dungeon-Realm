3c503fd14c3d556677a16936b59afcc3204c9d62
Stefano Peris <lordstephen77@gmail.com>
2017-10-21 06:17:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+* @name        Return of Dungeon
+* @copyright   Stefano Peris (c) 2017
+* @author      Stefano Peris
+* @email       lordstephen77@gmail.com
+* @github      https://github.com/LordStephen77/Return-of-Dungeon
+* @license     GPL-3.0 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+* @create      lun 21 ott 2017 06:18:43 CEST
+* @update      null
+"""
+
+import random
+import time
+
+def displayIntro():
+    print('You are in a land full of dragons. In front of you,')
+    print('you see two caves. In one cave, the dragon is friendly')
+    print('and will share his treasure with you. The other dragon')
+    print('is greedy and hungry, and will eat you on sight.')
+    print()
+
+def chooseCave():
+    cave = ''
+    while cave != '1' and cave != '2':
+        print('Which cave will you go into? (1 or 2)')
+        cave = input()
+
+    return cave
+
+def checkCave(chosenCave):
+    print('You approach the cave...')
+    time.sleep(2)
+    print('It is dark and spooky...')
+    time.sleep(2)
+    print('A large dragon jumps out in front of you! He opens his jaws and...')
+    print()
+    time.sleep(2)
+
+    friendlyCave = random.randint(1, 2)
+
+    if chosenCave == str(friendlyCave):
+         print('Gives you his treasure!')
+    else:
+         print('Gobbles you down in one bite!')
+
+playAgain = 'yes'
+while playAgain == 'yes' or playAgain == 'y':
+
+    displayIntro()
+
+    caveNumber = chooseCave()
+
+    checkCave(caveNumber)
+
+    print('Do you want to play again? (yes or no)')
+    playAgain = input()
+
+
